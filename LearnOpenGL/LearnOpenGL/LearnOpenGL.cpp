2427fd83fb6364899559c14735dcfa54eea9057e
Alex <alexcn711@gmail.com>
2022-10-05 09:19:54 -0600

    doing hello window

--- LearnOpenGL/LearnOpenGL/LearnOpenGL.cpp
+++ LearnOpenGL/LearnOpenGL/LearnOpenGL.cpp
@@ -5,6 +5,8 @@
 // The glad header must go before glfw to avoid errors
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <string>
+using namespace std;
 
 void instantiateGLFWWindow()
 {
@@ -14,8 +16,42 @@ void instantiateGLFWWindow()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
+int createGLFWWindow(int width, int height, string title)
+{
+    GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Error! Failed to create GLFW window!" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    return 0;
+}
+
+int loadGLAD()
+{
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Error! Failed to initialize GLAD!" << endl;
+        return - 1;
+    }
+    return 0;
+}
+
 int main()
 {
     std::cout << "Hello Learn OpenGL!\n";
     instantiateGLFWWindow();
+    int windowStatus = createGLFWWindow(800, 600, "OpenGL Window");
+    if (windowStatus == -1)
+    {
+        return -1;
+    }
+    int gladStatus = loadGLAD();
+    if (gladStatus == -1)
+    {
+        return -1;
+    }
+
 }
