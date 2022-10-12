a088c80b738bdf78c3feff07eb40d40d351f6f88
Alex <alexcn711@gmail.com>
2022-10-11 20:01:36 -0600

    working on hello triangle

--- LearnOpenGL/LearnOpenGL/LearnOpenGL.cpp
+++ LearnOpenGL/LearnOpenGL/LearnOpenGL.cpp
@@ -14,6 +14,14 @@ struct Window
     int status;
 };
 
+// Triangle vertex shader
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 apos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 void instantiateGLFWWindow()
 {
     glfwInit();
@@ -53,7 +61,7 @@ void setDimensionsOfRenderingWindow(int width, int height)
 }
 
 /*
-*   Adjusts the viewport when we resize the OpenGL window. 
+*   Adjusts the viewport when we resize the OpenGL window, so the rendered image is resized.
 */
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
@@ -71,12 +79,64 @@ void renderBlueGreenWindow()
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
+/*
+* Vertex buffer objects are an efficient way of sending large amounts of vertices to the GPU.
+*/
+unsigned int createVertexBufferObject(float triangleVertices[])
+{
+    // Defining our buffer
+    unsigned int vertexBufferObject;
+    glGenBuffers(1, &vertexBufferObject);
+    // Giving the buffer a type (array for the triangle vertices)
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    // Actually inserting the vertices into the buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+    return vertexBufferObject;
+}
+
+void checkForCompileTimeErrors(unsigned int vertexShader)
+{
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        cout << "Error: Building vertex shader failed!\n" << infoLog << endl;
+    }
+}
+
+unsigned int buildVertexShader()
+{
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach the shader code to the shader object and compile it
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkForCompileTimeErrors(vertexShader);
+    return vertexShader;
+}
+
+void renderTriangle()
+{
+    // Defining a 2D triangle with z-coordinates of 0.
+    float triangleVertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    unsigned int vertexBufferObject = createVertexBufferObject(triangleVertices);
+    unsigned int vertexShader = buildVertexShader();
+}
+
 void executeRenderCommands()
 {
     renderBlueGreenWindow();
+    renderTriangle();
 }
 
-void processInput(GLFWwindow* window)
+void processInputFromPeripherals(GLFWwindow* window)
 {
     // If ESC is hit, close the window.
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -89,7 +149,7 @@ void runRenderLoop(GLFWwindow* window)
 {
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        processInputFromPeripherals(window);
         executeRenderCommands();
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -121,5 +181,5 @@ int main()
     setDimensionsOfRenderingWindow(width, height);
     setUpAutomaticViewportAdjustment(window.window);
     runRenderLoop(window.window);
-    closeWindow();
+    return closeWindow();
 }
